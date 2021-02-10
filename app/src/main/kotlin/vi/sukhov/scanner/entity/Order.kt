c194531a4c8d3535f4764a5bd8cc8a49c94b2728
ViacheslavSukhov <svo_efremov@mail.ru>
2021-02-10 13:16:58 +0300

    Created OrderListAdapter

--- app/src/main/kotlin/vi/sukhov/scanner/entity/Order.kt
+++ app/src/main/kotlin/vi/sukhov/scanner/entity/Order.kt
@@ -2,7 +2,8 @@ package vi.sukhov.scanner.entity
 
 data class Order(
     val id: String?,
-    val title: String?,           // Name order or other
+    val title: String?,     // Name order or other
+    val code: String?,      // Order code
     val date: String?,
     val status: String?,
     val image: String? = null
---
+++ app/src/main/kotlin/vi/sukhov/scanner/ui/home/orders/adapter/OrderListAdapter.kt
@@ -0,0 +1,44 @@
+package vi.sukhov.scanner.ui.home.orders.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import vi.sukhov.scanner.databinding.ItemOrderBinding
+import vi.sukhov.scanner.entity.Order
+
+class OrderListAdapter(private val itemClickListener: View.OnClickListener) :
+    RecyclerView.Adapter<OrderListAdapter.OrderViewHolder>() {
+
+    private val list: ArrayList<Order> = arrayListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val itemPersonBinding = ItemOrderBinding.inflate(layoutInflater, parent, false)
+        return OrderViewHolder(itemPersonBinding)
+    }
+
+    override fun onBindViewHolder(holder: OrderViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    fun updateListAndNotify(list: List<Order>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    inner class OrderViewHolder(private val v: ItemOrderBinding) : RecyclerView.ViewHolder(v.root) {
+
+        fun bind(item: Order) {
+            v.title.text = item.title
+            v.code.text = item.code
+            v.status.text = item.status
+
+            itemView.setOnClickListener(itemClickListener)
+        }
+    }
+
+}
