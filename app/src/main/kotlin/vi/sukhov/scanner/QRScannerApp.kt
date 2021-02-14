8089358874041700ee468a5ae84c1c089480fef1
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-02-14 08:56:11 +0300

    Added functions starting DB's

--- app/src/main/kotlin/vi/sukhov/scanner/QRScannerApp.kt
+++ app/src/main/kotlin/vi/sukhov/scanner/QRScannerApp.kt
@@ -1,9 +1,28 @@
 package vi.sukhov.scanner
 
 import android.app.Application
+import androidx.room.RoomDatabase
+import com.google.firebase.database.FirebaseDatabase
 import dagger.hilt.android.HiltAndroidApp
-import vi.sukhov.scanner.util.Logger
-import javax.inject.Inject
+import vi.sukhov.scanner.data.local.database.room.OrdersDatabase
 
 @HiltAndroidApp
-class QRScannerApp : Application()
+class QRScannerApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startRoom()
+        startFirebaseDatabase()
+    }
+
+
+    private fun startRoom() {
+        OrdersDatabase.buildDatabase(applicationContext)
+    }
+
+    private fun startFirebaseDatabase() {
+        val database = FirebaseDatabase.getInstance()
+        database.setPersistenceEnabled(true)            // Кэширование db
+    }
+}
