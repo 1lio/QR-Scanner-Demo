f6bc7d4d00eeed3eb352ce571568ef72547752a3
Viacheslav Sukhov <svoefremov@yandex.ru>
2021-02-14 13:52:36 +0300

    Save user local

--- app/src/main/kotlin/vi/sukhov/scanner/data/local/prefs/DataStoreStorage.kt
+++ app/src/main/kotlin/vi/sukhov/scanner/data/local/prefs/DataStoreStorage.kt
@@ -2,13 +2,13 @@ package vi.sukhov.scanner.data.local.prefs
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.createDataStore
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
 import vi.sukhov.scanner.data.gateway.PreferenceStorage
+import vi.sukhov.scanner.entity.User
 import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -23,12 +23,31 @@ class DataStoreStorage @Inject constructor(context: Context) : PreferenceStorage
 
         val KEY_IS_SIGNED = booleanPreferencesKey("prefs_is_signed")
         val KEY_IS_DARK_MODE = booleanPreferencesKey("prefs_is_dark_mode")
-    }
 
+        object UserScheme {
+            val FIELD_ID = stringPreferencesKey("user_id")
+            val FIELD_NAME = stringPreferencesKey("user_name")
+        }
+    }
 
     override suspend fun isSigned(): Flow<Boolean> = dataStore.savePref(KEY_IS_SIGNED, false)
     override suspend fun saveSign(isSigned: Boolean) {
         dataStore.editPref(KEY_IS_SIGNED, isSigned)
+    }
+
+    override suspend fun getUser(): Flow<User> {
+        return dataStore.data.map {
+            val id: String? = it[UserScheme.FIELD_ID]
+            val name: String? = it[UserScheme.FIELD_NAME]
+            User(id!!, name)
+        }
+    }
+
+    override suspend fun saveUser(user: User) {
+        dataStore.edit { prefs ->
+            prefs[UserScheme.FIELD_ID] = user.id
+            prefs[UserScheme.FIELD_NAME] = user.name ?: ""
+        }
     }
 
     override suspend fun isDarkMode(): Flow<Boolean> = dataStore.savePref(KEY_IS_DARK_MODE, false)
--- app/src/main/kotlin/vi/sukhov/scanner/data/local/prefs/SharedPreferenceStorage.kt
+++ app/src/main/kotlin/vi/sukhov/scanner/data/local/prefs/SharedPreferenceStorage.kt
@@ -6,6 +6,7 @@ import androidx.core.content.edit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import vi.sukhov.scanner.data.gateway.PreferenceStorage
+import vi.sukhov.scanner.entity.User
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,6 +17,22 @@ class SharedPreferenceStorage @Inject constructor(context: Context) : Preference
         const val PREFS_NAME = "ru.sukhov.scanner"
         const val PREFS_IS_SIGNED = "prefs_is_signed"
         const val PREFS_IS_DARK_MODE = "prefs_is_dark_mode"
+        const val FIELD_ID = "user_id"
+        const val FIELD_NAME = "user_name"
+    }
+
+    override suspend fun getUser(): Flow<User> {
+        return flow {
+            val id: String? = prefs.value.getString(FIELD_ID, "")
+            val name: String? = prefs.value.getString(FIELD_NAME, "")
+
+            emit(User(id!!, name))
+        }
+    }
+
+    override suspend fun saveUser(user: User) {
+        prefs.value.edit { putString(FIELD_ID, user.id) }
+        prefs.value.edit { putString(FIELD_NAME, user.name) }
     }
 
     private val prefs: Lazy<SharedPreferences> = lazy {
