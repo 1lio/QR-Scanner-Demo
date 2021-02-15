92f2c4ec5dc3485cf0b58a41dd90580966d771bc
ViacheslavSukhov <svo_efremov@mail.ru>
2021-02-15 11:39:11 +0300

    Fix firebase chars

--- app/src/main/kotlin/vi/sukhov/scanner/data/remote/FirebaseOrderDatabase.kt
+++ app/src/main/kotlin/vi/sukhov/scanner/data/remote/FirebaseOrderDatabase.kt
@@ -5,9 +5,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.sendBlocking
 import kotlinx.coroutines.flow.Flow
@@ -15,7 +13,6 @@ import kotlinx.coroutines.flow.callbackFlow
 import vi.sukhov.scanner.data.gateway.OrdersStorage
 import vi.sukhov.scanner.entity.Order
 import java.util.*
-import kotlin.collections.ArrayList
 
 object FirebaseOrderDatabase : OrdersStorage {
 
@@ -36,34 +33,41 @@ object FirebaseOrderDatabase : OrdersStorage {
     @ExperimentalCoroutinesApi
     override suspend fun getOrder(id: String): Flow<Order> = callbackFlow {
 
-        val eventListener = orderRef.child(id)
-            .addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    snapshot.let {
-                        // Проблема с приведением типов. Упроно думает что string это лонг несмотря на кавычки в БД.
-                        // Решено пока так.
-                        val order = Order(
-                            id = it.key ?: notExistOrder.id,
-                            title = it.child("title").getValue(String::class.java)
-                                ?: notExistOrder.title,
-                            code = it.child("code").getValue(String::class.java)
-                                ?: notExistOrder.code,
-                            date = it.child("date").getValue(String::class.java)
-                                ?: notExistOrder.date,
-                            status = it.child("status").getValue(String::class.java)
-                                ?: notExistOrder.status,
-                            image = it.child("image").getValue(String::class.java)
-                                ?: notExistOrder.image,
-                        )
 
-                        this@callbackFlow.sendBlocking(order)
+        val re = Regex("[.,#$:// \\[\\]]")          // Firebase запрешает использовать данные символы
+        val pId: String = re.replace(id, "")
+
+        val eventListener =
+            orderRef.child(pId) // Данные символы запрещены в firebase
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        snapshot.let {
+
+                            // Проблема с приведением типов. Упроно думает что string это лонг несмотря на кавычки в БД.
+                            // Решено пока так.
+
+                            val order = Order(
+                                id = it.key ?: notExistOrder.id,
+                                title = it.child("title").getValue(String::class.java)
+                                    ?: notExistOrder.title,
+                                code = it.child("code").getValue(String::class.java)
+                                    ?: notExistOrder.code,
+                                date = it.child("date").getValue(String::class.java)
+                                    ?: notExistOrder.date,
+                                status = it.child("status").getValue(String::class.java)
+                                    ?: notExistOrder.status,
+                                image = it.child("image").getValue(String::class.java)
+                                    ?: notExistOrder.image,
+                            )
+
+                            this@callbackFlow.sendBlocking(order)
+                        }
                     }
-                }
 
-                override fun onCancelled(snapshot: DatabaseError) {
-                    this@callbackFlow.close(snapshot.toException())
-                }
-            })
+                    override fun onCancelled(snapshot: DatabaseError) {
+                        this@callbackFlow.close(snapshot.toException())
+                    }
+                })
 
         awaitClose {
             orderRef.removeEventListener(eventListener)
@@ -85,8 +89,8 @@ object FirebaseOrderDatabase : OrdersStorage {
 
     @ExperimentalCoroutinesApi
     override suspend fun removeOrder(order: Order) {
-            val id = order.id
-            if (id != null) repository.getReference(ORDERS_REFERENCE).child(id).removeValue()
+        val id = order.id
+        if (id != null) repository.getReference(ORDERS_REFERENCE).child(id).removeValue()
     }
 
     override fun removeAllOrders() {
